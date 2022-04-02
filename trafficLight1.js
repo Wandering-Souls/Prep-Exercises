fed2ee2da9da00eec996711bd5d94eec397a168f
Serva <101137198+Wandering-Souls@users.noreply.github.com>
2022-04-02 19:23:53 +0200

    Update trafficLight1.js

--- trafficLight1.js
+++ trafficLight1.js
@@ -18,3 +18,18 @@ while (rotations < 2) {
     rotations++;
   }
 }
+
+  // if the color is green, turn it orange
+  // if the color is orange, turn it red
+  // if the color is red, add 1 to rotations and turn it green
+
+
+/*
+  The output:
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+The traffic light is on green
+The traffic light is on orange
+The traffic light is on red
+*/
