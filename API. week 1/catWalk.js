b266031e4597b84a95194c56957cb07ddbfb3aa1
Serva <servas230@gmail.com>
2022-05-21 20:52:38 +0200

    Added function walk codes

--- API. week 1/catWalk.js
+++ API. week 1/catWalk.js
@@ -6,23 +6,40 @@ const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
 
-function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
+  // Resolve this promise when the cat (`img`) has walked from `startPos` to
     // `stopPos`.
     // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
     // constants.
+
+function walk(img, startPos, stopPos) {
+    let positionFromLeft = startPos;
+  return new Promise((resolve) => {
+ let stepInterval = setInterval( () =>{
+  if (positionFromLeft < stopPos){
+    positionFromLeft += STEP_SIZE_PX;
+    img.style.left = `{positionFromLeft}`;
+  } 
+}, STEP_INTERVAL_MS)
   });
+  
 }
 
-function dance(img) {
-  return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
+   // Switch the `.src` of the `img` from the walking cat to the dancing cat
     // and, after a timeout, reset the `img` back to the walking cat. Then
     // resolve the promise.
     // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+
+function dance(img) {
+  return new Promise((resolve) => {
+ 
   });
 }
+
+ // Use the `walk()` and `dance()` functions to let the cat do the following:
+  // 1. Walk from `startPos` to `centerPos`.
+  // 2. Then dance for 5 secs.
+  // 3. Then walk from `centerPos` to `stopPos`.
+  // 4. Repeat the first three steps indefinitely.
 
 function catWalk() {
   const img = document.querySelector('img');
@@ -30,11 +47,7 @@ function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+ 
 }
 
 window.addEventListener('load', catWalk);
